69d51cbdcdb891ce1ed7f98122e7918f7aa161db
Veeraldo <Veraldo24092005@gmail.com>
2025-01-10 18:20:33 +0700

    p

--- api/index.php
+++ api/index.php
@@ -2,3 +2,8 @@
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
+if (!file_exists($requestedFile)) {
+    http_response_code(404);
+    echo "Page Not Found";
+    exit;
+}
